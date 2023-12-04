bb0fcd7b5569c10a603ce6d9afc730d2633dffa9
damhyeong <rhdwhdals8@naver.com>
2023-12-05 01:55:11 +0900

    Complete Axios Function In UploadMainPage. Function Works for Sending Information To Spring.' '

--- src/components/UploadPage/UploadInfoContainer/UploadInfoContainer.tsx
+++ src/components/UploadPage/UploadInfoContainer/UploadInfoContainer.tsx
@@ -4,47 +4,54 @@ import LevelSelector from "../LevelSelector/LevelSelector";
 import ExampleInOutput from "../ExampleInOutput/ExampleInOutput";
 
 interface PIFace{
-    titleRef : React.MutableRefObject<string>;
-    levelRef : React.MutableRefObject<number>;
-    introduceRef : React.MutableRefObject<string>;
-    constraintsRef : React.MutableRefObject<string>;
-    examInputRef : React.MutableRefObject<string>;
-    examOutputRef : React.MutableRefObject<string>;
+    title : string;
+    setTitle : React.Dispatch<React.SetStateAction<string>>;
+    level : number;
+    setLevel : React.Dispatch<React.SetStateAction<number>>;
+    introduce : string;
+    setIntroduce : React.Dispatch<React.SetStateAction<string>>;
+    constraints : string;
+    setConstraints : React.Dispatch<React.SetStateAction<string>>;
+    examInput : string;
+    setExamInput : React.Dispatch<React.SetStateAction<string>>;
+    examOutput : string;
+    setExamOutput : React.Dispatch<React.SetStateAction<string>>;
 }
 
 const UploadInfoContainer =
-    ({titleRef, levelRef, introduceRef, constraintsRef, examInputRef, examOutputRef} : PIFace) => {
+    ({title, setTitle, level, setLevel, introduce, setIntroduce, constraints, setConstraints, examInput, setExamInput, examOutput, setExamOutput} : PIFace) => {
+
 
 
     const onChangeTitle = useCallback((e : React.ChangeEvent<HTMLInputElement>) => {
-        titleRef.current = e.target.value;
-    }, [titleRef]);
+        setTitle(e.target.value);
+    }, [setTitle]);
     const onClickLevel = useCallback((id : number) => {
-        levelRef.current = id;
-    }, [levelRef]);
+        setLevel(id);
+    }, [setLevel]);
     const onChangeIntroduce = useCallback((e : React.ChangeEvent<HTMLTextAreaElement>) => {
-        introduceRef.current = e.target.value
-    }, [introduceRef])
+        setIntroduce(e.target.value);
+    }, [setIntroduce])
     const onChangeConstraints = useCallback((e : React.ChangeEvent<HTMLTextAreaElement>) => {
-        constraintsRef.current = e.target.value;
-    }, [constraintsRef])
+        setConstraints(e.target.value);
+    }, [setConstraints])
     const onChangeInput = useCallback((e : React.ChangeEvent<HTMLTextAreaElement>) => {
-        examInputRef.current = e.target.value;
-    }, [examInputRef]);
+        setExamInput(e.target.value);
+    }, [setExamInput]);
     const onChangeOutput = useCallback((e : React.ChangeEvent<HTMLTextAreaElement>) => {
-        examOutputRef.current= e.target.value;
-    }, [examOutputRef]);
+        setExamOutput(e.target.value);
+    }, [setExamOutput]);
 
     return (
         <div className={"upload-info-container"}>
             <div className={"title-container"}>
                 <div className={"indicate-title"}>제목 : </div>
-                <input className={"input-title"} value={titleRef.current} onChange={onChangeTitle} placeholder={"문제 제목을 입력하세요."}/>
+                <input className={"input-title"} value={title} onChange={onChangeTitle} placeholder={"문제 제목을 입력하세요."}/>
             </div>
             <hr/>
             <div className={"indicate"}>Level</div>
             <div className={"select-level"}>
-                <LevelSelector onClickLevel={onClickLevel} level={levelRef.current}/>
+                <LevelSelector onClickLevel={onClickLevel} level={level}/>
             </div>
             <hr/>
             <div className={"indicate"}>문제 설명</div>
--- src/components/UploadPage/UploadMainPage/UploadMainPage.tsx
+++ src/components/UploadPage/UploadMainPage/UploadMainPage.tsx
@@ -1,4 +1,4 @@
-import React, {createRef, useCallback, useEffect, useRef} from "react";
+import React, {createRef, useCallback, useEffect, useRef, useState} from "react";
 import './styles.scss'
 import UploadInfoContainer from "../UploadInfoContainer/UploadInfoContainer";
 import axios from "axios";
@@ -15,49 +15,47 @@ interface UploadIFace{
 
 const UploadMainPage = () => {
 
-    const onClickUpload = useCallback(async (e : React.MouseEvent<HTMLButtonElement>) => {
+    const [title, setTitle] = useState<string>('');
+    const [level, setLevel] = useState<number>(0);
+    const [introduce, setIntroduce] = useState<string>('');
+    const [constraints, setConstraints] = useState<string>('');
+    const [examInput, setExamInput] = useState<string>('');
+    const [examOutput, setExamOutput] = useState<string>('');
+
+    const onClickUpload = useCallback(async (e : React.MouseEvent<HTMLDivElement>) => {
         e.preventDefault();
-        
-        /**
-         * 필수 - title, introduce, examOutput
-         * 문자열이 '' 일 때, 묻기
-         * - constraints(문제에 대해 더 이상 힌트를 주거나, 변수의 범위를 정할 필요가 없는지,
-         * - examInput(문제에 입력 값은 필요 없는지.
-         * 위의 두 조건에 대해 NOT NULL 표시를 해 놓았으므로, ALTER TABLE DDL을 통해 속성을 변경시킬 필요가 있어 보인다.
-         * - constraints가 '' : 빈 문자열일 경우, SolvePage Component에서 아예 display : none 속성을 적용시키는 것도 좋은 방법이다.
-         *
-         * const data = await axios.post("url");
-         * axios.post 를 통해 INSERT 문을 실행 시킬 때는 모든 것이 완벽 할 때 실행한다.
-         * */
 
-    }, []);
+        // 여기에 입력하지 않은 값이 있는지 확인하는 코드를 작성해야 완벽.
 
-    // useRef를 통해 하위 컴포넌트에 모든 정보를 묶어서 Props로 내려보낼 경우,
-    // 하나의 값을 제외한 모든 값을 다시 복사하는 비효율적인 상황이 연출된다.
-    // 추후 useReducer 적용을 통해 최적화를 하는 것도 좋은 방법이라 생각된다.
-    const titleRef = useRef<string>('');
-    const levelRef = useRef<number>(0);
-    const introduceRef = useRef<string>('');
-    const constraintsRef = useRef<string>('');
-    const examInputRef = useRef<string>('');
-    const examOutputRef = useRef<string>('');
+        const payload = {
+            title, level, introduce, constraints, examInput, examOutput
+        }
+
+        try{
+            const response = await axios.post("http://localhost:4000/api/examples/examUpload", payload);
+            console.log("response : " + response);
+        } catch (e){
+            console.log(e);
+        }
+
+    }, [title, level, introduce, constraints, examInput, examOutput]);
 
     return (
         <div className={"upload-main-page"}>
             <div className={"upload-page-header"}>
                 <div className={"project-title"}>Data Creature</div>
-                <div className={"upload-button"}>Upload</div>
+                <div className={"upload-button"} onClick={onClickUpload}>Upload</div>
             </div>
             <hr/>
             <div className={"upload-page-background"}>
                 <div className={"upload-page-container"}>
                     <UploadInfoContainer
-                        titleRef={titleRef}
-                        levelRef={levelRef}
-                        introduceRef={introduceRef}
-                        constraintsRef={constraintsRef}
-                        examInputRef={examInputRef}
-                        examOutputRef={examOutputRef}
+                        title={title} setTitle={setTitle}
+                        level={level} setLevel={setLevel}
+                        introduce={introduce} setIntroduce={setIntroduce}
+                        constraints={constraints} setConstraints={setConstraints}
+                        examInput={examInput} setExamInput={setExamInput}
+                        examOutput={examOutput} setExamOutput={setExamOutput}
                     />
                 </div>
             </div>
