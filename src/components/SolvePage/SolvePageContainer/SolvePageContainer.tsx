30a935f6b511dc8558557bac387dd353d4e70864
damhyeong <rhdwhdals8@naver.com>
2023-12-05 10:48:15 +0900

    Commit For saving Files

--- src/components/SolvePage/SolvePageContainer/SolvePageContainer.tsx
+++ src/components/SolvePage/SolvePageContainer/SolvePageContainer.tsx
@@ -29,28 +29,10 @@ const SolvePageContainer = () => {
     };
 
     // Get the current query parameters
-    const queryParams = getQueryParams(location.search)
+    const queryParams = getQueryParams(location.search);
 
-    /*
-    추후 이 영역에 문제 상세 정보에 대해서 가져와야 한다. (Database -> Backend -> Frontend)
-    axios 사용하기
-    private int exam_number;
-    private String introduce;
-    private String exam_constraints;
-    private String exam_input;
-    private String exam_output;
 
-    const sampleDetailData = {
-        exam_number : 3,
-        introduce : "JadenCase란 모든 단어의 첫 문자가 대문자이고, 그 외의 알파벳은 소문자인 문자열입니다.\n단, 첫 문자가 알파벳이 아닐 때에는 이어지는 알파벳은 소문자로 쓰면 됩니다.",
-        exam_constraints : "* 'input'은 길이 1 이상 200 이하인 문자열입니다.\n* 'input'은 알파벳과 숫자, 공백문자(' ')로 이루어져 있습니다.\n* 숫자는 단어의 첫 문자로만 나옵니다.\n* 숫자로만 이루어진 단어는 없습니다.",
-        exam_input : "3people unFollwed me",
-        exam_output : "3people Unfollwed Me",
-    }
 
-    // 문제 상세 정보를 데이터베이스에서 받아온다면 딱 이 모양.
-    const {exam_number, introduce, exam_constraints, exam_input, exam_output} = sampleDetailData;
-*/
 
     // API에서 데이터를 가져오기 위한 상태
     const [detailData, setDetailData] = useState({
@@ -102,7 +84,7 @@ const SolvePageContainer = () => {
 
     // RunArea에 넘길 함수. -- "RUN!' 버튼 클릭 시 구동
     const onClickRunButton = useCallback(async () => {
-        const response = await axios.post('http://localhost:4000/execute-code', code);
+        const response = await axios.post('http://localhost:4000/docker/execute-code', code);
 
         console.log(response.data);
 
